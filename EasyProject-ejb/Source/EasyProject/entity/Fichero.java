2fd89936e0bf6610786fc22e25a20393151e5cbb
vvyycc <victorroldan18@gmail.com>
2016-03-02 11:01:30 +0100

    JPA y EJB

---
+++ EasyProject-ejb/Source/EasyProject/entity/Fichero.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EasyProject.entity;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author victo
+ */
+@Entity
+@Table(name = "FICHERO")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Fichero.findAll", query = "SELECT f FROM Fichero f"),
+    @NamedQuery(name = "Fichero.findByIdFichero", query = "SELECT f FROM Fichero f WHERE f.idFichero = :idFichero"),
+    @NamedQuery(name = "Fichero.findByRuta", query = "SELECT f FROM Fichero f WHERE f.ruta = :ruta")})
+public class Fichero implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID_FICHERO")
+    private Long idFichero;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "RUTA")
+    private String ruta;
+    @JoinColumn(name = "ID_TAREA", referencedColumnName = "ID_TAREA")
+    @ManyToOne(optional = false)
+    private Tarea idTarea;
+
+    public Fichero() {
+    }
+
+    public Fichero(Long idFichero) {
+        this.idFichero = idFichero;
+    }
+
+    public Fichero(Long idFichero, String ruta) {
+        this.idFichero = idFichero;
+        this.ruta = ruta;
+    }
+
+    public Long getIdFichero() {
+        return idFichero;
+    }
+
+    public void setIdFichero(Long idFichero) {
+        this.idFichero = idFichero;
+    }
+
+    public String getRuta() {
+        return ruta;
+    }
+
+    public void setRuta(String ruta) {
+        this.ruta = ruta;
+    }
+
+    public Tarea getIdTarea() {
+        return idTarea;
+    }
+
+    public void setIdTarea(Tarea idTarea) {
+        this.idTarea = idTarea;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idFichero != null ? idFichero.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Fichero)) {
+            return false;
+        }
+        Fichero other = (Fichero) object;
+        if ((this.idFichero == null && other.idFichero != null) || (this.idFichero != null && !this.idFichero.equals(other.idFichero))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "EasyProject.entity.Fichero[ idFichero=" + idFichero + " ]";
+    }
+    
+}
---
+++ EasyProject-ejb/Source/EasyProject/entity/Usuario.java
@@ -0,0 +1,163 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package EasyProject.entity;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
+
+/**
+ *
+ * @author victo
+ */
+@Entity
+@Table(name = "USUARIO")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Usuario.findAll", query = "SELECT u FROM Usuario u"),
+    @NamedQuery(name = "Usuario.findByIdUsuario", query = "SELECT u FROM Usuario u WHERE u.idUsuario = :idUsuario"),
+    @NamedQuery(name = "Usuario.findByEmail", query = "SELECT u FROM Usuario u WHERE u.email = :email"),
+    @NamedQuery(name = "Usuario.findByToken", query = "SELECT u FROM Usuario u WHERE u.token = :token")})
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "ID_USUARIO")
+    private Long idUsuario;
+    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="Invalid email")//if the field contains email address consider using this annotation to enforce field validation
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "EMAIL")
+    private String email;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 20)
+    @Column(name = "TOKEN")
+    private String token;
+    @ManyToMany(mappedBy = "usuarioCollection")
+    private Collection<Tarea> tareaCollection;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idUsuario1")
+    private Collection<Comentario> comentarioCollection;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idUsuario")
+    private Collection<Proyecto> proyectoCollection;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idUsuario")
+    private Collection<Tarea> tareaCollection1;
+
+    public Usuario() {
+    }
+
+    public Usuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public Usuario(Long idUsuario, String email, String token) {
+        this.idUsuario = idUsuario;
+        this.email = email;
+        this.token = token;
+    }
+
+    public Long getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(Long idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @XmlTransient
+    public Collection<Tarea> getTareaCollection() {
+        return tareaCollection;
+    }
+
+    public void setTareaCollection(Collection<Tarea> tareaCollection) {
+        this.tareaCollection = tareaCollection;
+    }
+
+    @XmlTransient
+    public Collection<Comentario> getComentarioCollection() {
+        return comentarioCollection;
+    }
+
+    public void setComentarioCollection(Collection<Comentario> comentarioCollection) {
+        this.comentarioCollection = comentarioCollection;
+    }
+
+    @XmlTransient
+    public Collection<Proyecto> getProyectoCollection() {
+        return proyectoCollection;
+    }
+
+    public void setProyectoCollection(Collection<Proyecto> proyectoCollection) {
+        this.proyectoCollection = proyectoCollection;
+    }
+
+    @XmlTransient
+    public Collection<Tarea> getTareaCollection1() {
+        return tareaCollection1;
+    }
+
+    public void setTareaCollection1(Collection<Tarea> tareaCollection1) {
+        this.tareaCollection1 = tareaCollection1;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (idUsuario != null ? idUsuario.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Usuario)) {
+            return false;
+        }
+        Usuario other = (Usuario) object;
+        if ((this.idUsuario == null && other.idUsuario != null) || (this.idUsuario != null && !this.idUsuario.equals(other.idUsuario))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "EasyProject.entity.Usuario[ idUsuario=" + idUsuario + " ]";
+    }
+    
+}
