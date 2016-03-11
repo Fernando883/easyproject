0450bf3d8b6792eb0d8bf5d3e8a449995a59c8e2
inftel11 <inftel11@192.168.183.81>
2016-03-11 09:17:04 +0100

    Carpetas borradas y algunas cosas corregidas

--- EasyProject-war/src/java/easyproject/beans/ProjectBean.java
+++ EasyProject-war/src/java/easyproject/beans/ProjectBean.java
@@ -22,7 +22,7 @@ import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
-import org.example.model.SendMail;
+import easyproject.model.SendMail;
 
 /**
  *
